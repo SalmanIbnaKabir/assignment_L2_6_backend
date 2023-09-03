19b9f20d1d9d26ae8d8ac9bd59d37a08d37822bf
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2023-09-03 23:42:19 +0600

    mongodb all domain add

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
-
+// middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.URI;
-// console.log(uri);
+// user: dbuser2
+// pass: AePiAMx4t5uNW1BB
+
+const uri =
+  "mongodb+srv://pc_builder:Hh8NbTrPrUOVY5le@cluster0.yjrqbh5.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-const run = async () => {
+async function run() {
   try {
-    await client.connect();
-    const db = await client.db("pc-builder");
-    const productCollection = await db.collection("product");
+    const productCollection = client.db("pc-builder").collection("product");
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
@@ -34,14 +34,14 @@ const run = async () => {
       const id = req.params.id;
 
       const result = await productCollection.findOne({ _id: new ObjectId(id) });
-      // console.log(result);
+
       res.send(result);
     });
     app.get("/category/:category", async (req, res) => {
       const category = req.params.category;
 
       const formattedCategory = category.split("-").join(" ");
-      // console.log(formattedCategory);
+
       const result = await productCollection
         .find({ category: { $regex: new RegExp(formattedCategory, "i") } })
         .toArray();
@@ -53,12 +53,8 @@ const run = async () => {
           .json({ error: `No products found in the category: ${category}` });
       }
     });
+
     app.get("/category", async (req, res) => {
-      // const formattedCategory = category.split("-").join(" ");
-      // console.log(formattedCategory);
-      // const result = await productCollection
-      //   .find({ category: { $regex: new RegExp(formattedCategory, "i") } })
-      //   .toArray();
       const result = await productCollection.distinct("category");
       if (result.length > 0) {
         res.json(result);
@@ -69,15 +65,16 @@ const run = async () => {
       }
     });
   } finally {
+    // await client.close();
   }
-};
+}
 
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello from node mongo crud server");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Listening to port ${port}`);
 });
